bc174c02962a7b63170a63861ec4a568d2049f9e
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-08-22 21:03:33 +0600

    added profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,32 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <>
+      <h1 className="text-center">Profile</h1>
+
+      <Form className="p-5 ">
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Your name" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Age</Form.Label>
+          <Form.Control type="number" placeholder="YOur age" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
 };
