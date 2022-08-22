3c60d606d67e864175a44cf957eb2a2a2a46a7b6
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-08-22 19:36:45 +0600

    added dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Profile } from './pages/Profile';
 
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </>
+  );
 };
