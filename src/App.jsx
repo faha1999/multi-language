98c4fa254ff19ed0dd0f8d4c5607c5575a8f7457
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-08-22 20:12:23 +0600

    added nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { Header } from './components/Header';
 import { Home } from './pages/Home';
 import { Profile } from './pages/Profile';
 
 export const App = () => {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <Navbar bg="light" expand="md">
+        <Container>
+          <Navbar.Brand href="#home">Multi Language site</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto" variant="pills" defaultActiveKey="/home">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/profile">Profile</Nav.Link>
+
+              <select className="nav-link bd-dark border-0 me-1">
+                <option value="en">English</option>
+                <option value="bn">Bangle</option>
+              </select>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 };
